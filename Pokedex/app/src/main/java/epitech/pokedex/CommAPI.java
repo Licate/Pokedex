d181702aac61170ea877ecf8c510962b7d2a503e
jybateman <jeanyves.bateman@gmail.com>
2017-04-27 17:11:11 +0200

    Added Api communication class

---
+++ Pokedex/app/src/main/java/epitech/pokedex/CommAPI.java
@@ -0,0 +1,16 @@
+package epitech.pokedex;
+
+import okhttp3.OkHttpClient;
+
+/**
+ * Created by jean-yves on 4/27/17.
+ */
+
+public class CommAPI {
+    private static String url = "http://pokeapi.co/api/v2/";
+    private static OkHttpClient client = new OkHttpClient();
+
+    void GetPokemons() {
+
+    }
+}
