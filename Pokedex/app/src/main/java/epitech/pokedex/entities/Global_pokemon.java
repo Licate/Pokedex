12983186588733ccc1ec6916f4184cfe071e29e6
Manon Delanoy <manon.delanoy@epitech.eu>
2017-04-30 18:05:19 +0200

    New entities - global list

---
+++ Pokedex/app/src/main/java/epitech/pokedex/entities/Global_pokemon.java
@@ -0,0 +1,32 @@
+package epitech.pokedex.entities;
+
+public class Global_pokemon {
+
+    private int id;
+    private String name;
+    private String default_sprite;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDefault_sprite() {
+        return default_sprite;
+    }
+
+    public void setDefault_sprite(String default_sprite) {
+        this.default_sprite = default_sprite;
+    }
+}
