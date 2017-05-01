55728f738005f7ff9d8a8ce5ad36bba84593176f
jybateman <jeanyves.bateman@gmail.com>
2017-05-01 19:30:32 +0200

    Made getpokemon an async method

--- Pokedex/app/src/main/java/epitech/pokedex/CommAPI.java
+++ Pokedex/app/src/main/java/epitech/pokedex/CommAPI.java
@@ -11,6 +11,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.json.JSONArray;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
@@ -32,9 +33,10 @@ import static android.widget.Toast.LENGTH_LONG;
 public class CommAPI {
     private static String url = "http://pokeapi.co/api/v2/";
     private static OkHttpClient client = new OkHttpClient();
-    ObjectMapper mapper = new ObjectMapper();
+    private ObjectMapper mapper = new ObjectMapper();
 
-    public List<GlobalPokemon> GetPokemons() {
+
+    /*public List<GlobalPokemon> GetPokemons() {
         List<GlobalPokemon> pokemons = null;
 
         try {
@@ -77,6 +79,52 @@ public class CommAPI {
                 e.printStackTrace();
             }
             return null;
+        }
+    }*/
+
+    public class GetPokemon extends AsyncTask<Void, Void, List<GlobalPokemon>> {
+
+        private String getRequest(String param) {
+            Request request = new Request.Builder()
+                    .url(param)
+                    .build();
+            Response response;
+            try {
+                response = client.newCall(request).execute();
+                return response.body().string();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected List<GlobalPokemon> doInBackground(Void... params) {
+            List<GlobalPokemon> pokemons = new ArrayList<GlobalPokemon>();
+
+            try {
+                JsonNode res;
+                JsonNode obj = mapper.readTree(this.getRequest(url + "pokemon"));
+                for (String next = obj.path("next").textValue(); next != null; next = obj.path("next").textValue()) {
+                    res = obj.path("results");
+                    if (res.isArray()) {
+                        for (final JsonNode objNode : res) {
+                            JsonNode pokenode = mapper.readTree(this.getRequest(objNode.path("url").asText()));
+                            GlobalPokemon tmp = new GlobalPokemon();
+                            JsonNode spritenode = pokenode.path("sprites");
+                            tmp.setDefault_sprite(spritenode.path("front_default").asText());
+                            tmp.setId(pokenode.path("id").asInt());
+                            tmp.setName(pokenode.path("name").asText());
+                            pokemons.add(tmp);
+                        }
+                    }
+                    obj = mapper.readTree(this.getRequest(next));
+                }
+                return pokemons;
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
         }
     }
 }
