e4fc0124681bdd87fb9b69f4c74bdd9d64a3d601
jybateman <jeanyves.bateman@gmail.com>
2017-05-04 00:45:00 +0200

    Added get detailed information for pokemons

--- Pokedex/app/src/main/java/epitech/pokedex/apicomms/GetPokemon.java
+++ Pokedex/app/src/main/java/epitech/pokedex/apicomms/GetPokemon.java
@@ -4,10 +4,12 @@ import android.os.AsyncTask;
 
 import com.fasterxml.jackson.databind.JsonNode;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 import epitech.pokedex.entities.GlobalPokemon;
+import epitech.pokedex.entities.Pokemon;
 
 /**
  * Created by jean-yves on 5/3/17.
@@ -43,6 +45,32 @@ public class GetPokemon extends CommAPI {
                 e.printStackTrace();
                 return null;
             }
+        }
+    }
+
+    public class Detail extends AsyncTask<String, Void, Pokemon> {
+        @Override
+        protected Pokemon doInBackground(String... params) {
+            Pokemon pokemon = new Pokemon();
+            try {
+                JsonNode obj = mapper.readTree(getRequest(url + "pokemon/" + params[0]));
+                pokemon.setId(obj.path("id").asInt());
+                pokemon.setName(obj.path("name").asText());
+                JsonNode typenodes = obj.path("types");
+                ArrayList<String> types = new ArrayList<String>();
+                for (JsonNode type : typenodes) {
+                    types.add(type.path("name").asText());
+                }
+                pokemon.setTypes_name(types);
+                JsonNode sprites = obj.path("sprites");
+                pokemon.setDefault_sprite(sprites.path("front_default").asText());
+                pokemon.setHeight(obj.path("height").asInt());
+                pokemon.setWeight(obj.path("weight").asInt());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return pokemon;
         }
     }
 }
